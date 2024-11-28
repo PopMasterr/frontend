1035d453cf9485e54800e7bbb4d1a2f35544adec
BallingBriedis <dominykas.pronskus@mif.stud.vu.lt>
2024-11-28 22:48:45 +0200

    Stilizavimo pushas
    navbar.js sukuriau hamburger mygtuka ekranams su ">688px".
    Paspaudus hamburgeri .navbar isilgeja, jame vidury rodomi paprasti .rightside navbar mygtukai.
    Rightside mygtuku yra 4 prisijungus ir ne, tai as naudoju set sizes.
    Pats .navbar irgi set size at 70px. Kitu puslapiu dydziai priklauso nuo navbaro (pvz.: login.css padariau min-height: calc(100vh - 70px);)
    Dar tvarkysiu profile, different mobile sizes suformatuosiu .CSS.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,6 +7,7 @@ import Cookies from "js-cookie";
 
 function Navbar() {
   const { logged, setLogged } = useContext(LoginContext);
+  const [menuOpen, setMenuOpen] = useState(false);
   const navigate = useNavigate();
   const apiUrl = process.env.REACT_APP_API + "api/auth/logout";
 
@@ -39,6 +40,14 @@ function Navbar() {
     navigate("/");
   }
 
+  function toggleMenu() {
+    setMenuOpen(!menuOpen);
+    const navbar = document.querySelector('.navbar');
+    if (navbar) {
+    navbar.classList.toggle('expanded', !menuOpen);
+    }
+  }
+
   return (
     <div className="navbar">
       <div className="leftSide">
@@ -48,21 +57,36 @@ function Navbar() {
         </Link>
       </div>
       <div className="rightSide">
-        <Link to="/">Home</Link>
-        <Link to="/play">Play</Link>
+      <button className="hamburger" onClick={toggleMenu}>
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+      </button>
+        <Link className="rightButton" to="/">Home</Link>
+        <Link className="rightButton" to="/play">Play</Link>
         {!Cookies.get("AuthToken") ? (
-          <Link to="/login">Log in</Link>
+          <Link className="rightButton" to="/login">Log in</Link>
         ) : (
-          <Link to="/profile">Profile</Link>
+          <Link className="rightButton" to="/profile">Profile</Link>
         )}
-        <Link
-          id="button"
-          to={!Cookies.get("AuthToken") ? "/signup" : "/"}
-          onClick={handleLogout}
-        >
+        <Link className="rightButton" id="button" to={!Cookies.get("AuthToken") ? "/signup" : "/"} onClick={handleLogout}>
           {!Cookies.get("AuthToken") ? "Sign Up" : "Log out"}
         </Link>
+        
       </div>
+      <div className={menuOpen ? "mobileMenu" : "mobileMenu hidden"}>
+        <Link className="rightButton2" to="/">Home</Link> 
+        <Link className="rightButton2" to="/play">Play</Link>
+        {!Cookies.get("AuthToken") ? (
+          <Link className="rightButton2" to="/login">Login</Link>
+        ) : (
+          <Link className="rightButton2" to="/profile">Profile</Link>
+        )}
+        <Link className="rightButton2" to={!Cookies.get("AuthToken") ? "/signup" : "/"}
+          onClick={handleLogout}>
+            {!Cookies.get("AuthToken") ? "SignUp" : "Logout"}
+            </Link>
+        </div>
     </div>
   );
 }
